c28d637fb5e726c9e9fbd83eb09e2a6026424f5a
Mojid Eroje <majidy287@gmail.com>
2023-08-22 16:41:31 +0100

    first commit

---
+++ Render-A-Component-Props/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Product from './Product'
+
+function App() {
+  return <Product name="iPhone 14 Pro Max" price = {1399} rating = "4.7/5.0" />;
+}
+
+export default App;
